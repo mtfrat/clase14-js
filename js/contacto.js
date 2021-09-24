a3d212fb220ef9732b622aa5be77d65367dd3aba
mtfrat <martinfraticelli@gmail.com>
2021-09-24 12:09:35 -0300

    muestro solo 1 vez al presionar boton

--- js/contacto.js
+++ js/contacto.js
@@ -1,22 +1,25 @@
 $(()=>{
 
     let divContacto = document.getElementById("divContacto")
+    let verificador = 0
 
     document.getElementById("botonContacto").addEventListener("click", () => {
-        fetch('../sucursales.json')
-        .then(response => response.json())
-        .then(data => {
-            divContacto.innerHTML += `<h2>Encontranos en:</h2>`
-            for (sucursal of data) {
-                divContacto.innerHTML += `
-                    <div id="${sucursal._id}" class="sucursales"> 
-                        <p>Dirección: ${sucursal.address} </p>
-                        <p>Mail: ${sucursal.email} </p>
-                        <p>Teléfono: ${sucursal.phone} </p>
-                    </div>
-                ` 
+        $.get("../sucursales.json", (respuesta,estado) => {
+            if (estado === "success") {
+                while(verificador !==1){
+                    divContacto.innerHTML += `<h2>Encontranos en:</h2>`
+                    for (sucursal of respuesta) {
+                        divContacto.innerHTML += `
+                            <div id="${sucursal._id}" class="sucursales"> 
+                                <p>Dirección: ${sucursal.address} </p>
+                                <p>Mail: ${sucursal.email} </p>
+                                <p>Teléfono: ${sucursal.phone} </p>
+                            </div>
+                        ` 
+                    }
+                    verificador = 1
+                }
             }
-            
         })
     })
 })
