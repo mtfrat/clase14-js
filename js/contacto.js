134d0bd62eb694628543f5f1faa63c777510113e
mtfrat <martinfraticelli@gmail.com>
2021-09-24 12:25:11 -0300

    probado con web server for chrome

--- js/contacto.js
+++ js/contacto.js
@@ -4,13 +4,13 @@ $(()=>{
     let verificador = 0
 
     document.getElementById("botonContacto").addEventListener("click", () => {
-        $.get("../sucursales.json", (respuesta,estado) => {
+        $.get("sucursales.json", (respuesta,estado) => {
             if (estado === "success") {
                 while(verificador !==1){
                     divContacto.innerHTML += `<h2>Encontranos en:</h2>`
                     for (sucursal of respuesta) {
                         divContacto.innerHTML += `
-                            <div id="${sucursal._id}" class="sucursales"> 
+                            <div class="sucursales"> 
                                 <p>Dirección: ${sucursal.address} </p>
                                 <p>Mail: ${sucursal.email} </p>
                                 <p>Teléfono: ${sucursal.phone} </p>
